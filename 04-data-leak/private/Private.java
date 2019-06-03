e031ecdab128c80d4f593f68ce3c787b7ed88e40
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2019-06-03 22:59:49 +0300

    04-data-leak: Complete private challenge

--- 04-data-leak/private/Private.java
+++ 04-data-leak/private/Private.java
@@ -1,4 +1,5 @@
-import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.util.Date;
 
@@ -6,21 +7,34 @@ class Sensitive {
     private Date d;
 
     public Sensitive() {
-        d = new Date();
+        d = new Date(1559591551L*1000);
     }
 
     public Date getDate() {
         return d;
     }
 
-    public void changePassword(String password) {
+}
+
+class Password {
+    public static boolean checkPassword(String password) {
+        try {
+            FileReader fin = openPasswordFile();
+            BufferedReader inStream = new BufferedReader(fin);
+            String p = inStream.readLine();
+            return p.equals(password);
+        } catch (Exception e) {
+        }
+        return false;
     }
 
-    public static FileInputStream openPasswordFile()
+    public static FileReader openPasswordFile()
             throws FileNotFoundException {
-            final String password_file = "password";
+            final String password_file = "password.txt";
+
             // do permission checks
-            FileInputStream fin = new FileInputStream(password_file);
+
+            FileReader fin = new FileReader(password_file);
             return fin;
     }
 }
@@ -28,5 +42,8 @@ class Sensitive {
 public class Private {
     public static void main(String[] args) {
         Sensitive s = new Sensitive();
+        Date d = s.getDate();
+        System.out.println("date is " + d);
+        System.out.println("password is alphaomega: " + Password.checkPassword("alphaomega"));
     }
 }
