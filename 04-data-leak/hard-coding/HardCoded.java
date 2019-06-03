257a011ae4fca5c5bf37028f8329d0111f064d4d
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2019-06-03 23:09:58 +0300

    04-data-leak: Add hard-coded challenge

---
+++ 04-data-leak/hard-coding/HardCoded.java
@@ -0,0 +1,6 @@
+public class HardCoded {
+    static String ipAddress = new String("141.85.224.117");
+    public static void main(String[] args) {
+        System.out.println("IP address is " + ipAddress);
+    }
+}
