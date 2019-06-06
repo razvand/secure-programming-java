33537f6b1e3f5883ec34ac83393a2707d1f68a33
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2019-06-06 10:48:19 +0300

    07-integers: Add big-integer challenge

--- 07-integers/big-integer/MyBigInteger.java
+++ 07-integers/big-integer/MyBigInteger.java
@@ -1,4 +1,9 @@
 public class MyBigInteger {
     public static void main(String[] args) {
+        int i1 = 0x00010000;
+        int i2 = 0x00020000;
+        int i3 = i1 * i2;
+        System.out.format("i3: 0x08x (%1$d)\n", i3, i3);
+        /* TODO: Use BigInteger. */
     }
 }
