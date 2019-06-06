6e9422e3b85d0e640d604e49c8cef59f3a9509e5
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2019-06-06 10:52:33 +0300

    07-integers: Add floating-point challenge

--- 07-integers/floating-point/FloatingPoint.java
+++ 07-integers/floating-point/FloatingPoint.java
@@ -1,4 +1,8 @@
 public class FloatingPoint {
     public static void main(String[] args) {
+        double dollar = 1.00;
+        double dime = 0.10;
+        int number = 7;
+        System.out.println("A dollar less " + number + " dimes is $" + (dollar - number * dime));
     }
 }
